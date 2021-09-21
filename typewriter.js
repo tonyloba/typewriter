bca44c6848733aca667c8eee50cc20b9f1bf40b2
tonyloba <tonyloba@gmail.com>
2021-09-21 21:25:13 +0000

    add typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,22 @@
-const sentence = "hello there from lighthouse labs";
+let delay = 0
+const sentence = (str) => {  
+  for (let i=0; i < str.length; i++) { 
+   delay += 50;
+    setTimeout(() => {
+      process.stdout.write(str[i]);      
+    },delay)    
+  } 
+  // process.stdout.write('\n');
+}
+setTimeout(() => {
+  console.log('')      
+},delay)   
+
+
+sentence("hello there from lighthouse labs\n");
+sentence("Another line of code");
+
+
+  //console.log(char);  // prints each in new line using in-built \n
+  // process.stdout.write(char);  // prints each in same line 
+
